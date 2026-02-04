5b48ceeedb9b76943b56fbd75f316d134f3c7081
Princy <princy@Princys-MacBook-Air.local>
2026-02-04 18:44:36 +0530

    dependency

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="princy",
+    author_email="princyshekhaliya2512@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
